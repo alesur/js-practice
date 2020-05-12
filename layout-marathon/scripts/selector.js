ee529dc06d502a020440486f6843d874c84b1d26
alesur <aleksandr.surkov@outlook.com>
2020-05-12 16:31:37 +0300

    tab-functionality-done

--- layout-marathon/scripts/selector.js
+++ layout-marathon/scripts/selector.js
@@ -1,19 +1,41 @@
+//Size selector
+
 $('button').on('click', function() {
     $('button').removeClass('selected');
     $(this).addClass('selected');
 });
 
-$(document).ready(function() {
+// Color selector
 
+$(document).ready(function() {
 
     $('.info__color input').on('click', function() {
 
         var tshirtColor = $(this).attr('data-image');
-        console.log(tshirtColor);
 
         $('.product-selector__img--active').removeClass('product-selector__img--active');
         $('.product-selector__item img[data-image = ' + tshirtColor + ']').addClass('product-selector__img--active');
         $(this).addClass('product-selector__img--active');
+    });
+
+});
+
+//Tab selector
+
+$(document).ready(function() {
+
+    $('.filter__item').on('click', function(e) {
+        e.preventDefault();
+
+        var tabSelected = $(this).attr('data');
+
+        $('.filter__item--active').removeClass('filter__item--active');
+        $('.tabcontent--active').removeClass('tabcontent--active');
+
+        $('.filter__item[data = ' + tabSelected + ']').addClass('filter__item--active');
+        $('.tabcontent[data = ' + tabSelected + ']').addClass('tabcontent--active');
+
+        // $(this).addClass('filter__item--active');
     });
 
 });
